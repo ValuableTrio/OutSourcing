5df5f21bf56256bfa939f18756ad6bdb70a3aca9
yallo428 <yallo428@naver.com>
2024-02-23 17:56:21 +0900

    init

---
+++ src/test/java/org/example/outsourcing/OutSourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.outsourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutSourcingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
