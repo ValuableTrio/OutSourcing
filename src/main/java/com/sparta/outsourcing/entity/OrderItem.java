98c7e8e7a3a13c65196d3dd62f5624a06a44e80e
ParkDongSeob <bagd0445@gmail.com>
2024-02-26 22:20:21 +0900

    Refactor code for improved readability and efficiency

--- src/main/java/com/sparta/outsourcing/entity/OrderItem.java
+++ src/main/java/com/sparta/outsourcing/entity/OrderItem.java
@@ -3,10 +3,8 @@ package com.sparta.outsourcing.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Entity
 @NoArgsConstructor
 @Table(name = "order_item")
