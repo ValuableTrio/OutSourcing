6052c5d0f233ecf97268701c499935b812cea778
Modafi <dkdpdldjdy0619@gmail.com>
2024-02-26 23:09:41 +0900

    feat: implement controller
    - OwnerController
    - UserController

--- src/main/java/com/sparta/outsourcing/controller/OwnerController.java
+++ src/main/java/com/sparta/outsourcing/controller/OwnerController.java
@@ -1,4 +1,49 @@
 package com.sparta.outsourcing.controller;
 
+import com.sparta.outsourcing.dto.owner.*;
+import com.sparta.outsourcing.service.OwnerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/owner")
+@RequiredArgsConstructor
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody SignupOwnerRequestDto dto) {
+        return ResponseEntity.ok().body(ownerService.signup(dto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginOwnerRequestDto dto) {
+        return ResponseEntity.ok().body(ownerService.login(dto));
+    }
+
+    @PutMapping("/{ownerId}")
+    public ResponseEntity<?> updateOwner(
+            @PathVariable Long ownerId,
+            @RequestBody UpdateOwnerRequestDto dto
+    ) {
+        return ResponseEntity.ok().body(ownerService.update(ownerId, dto));
+    }
+
+    @PutMapping("business-number/{ownerId}")
+    public ResponseEntity<?> updateBusinessNumber(
+            @PathVariable Long ownerId,
+            @RequestBody UpdateOwnerBusinessNumberRequestDto dto
+    ) {
+        return ResponseEntity.ok().body(ownerService.updateBusinessNumber(ownerId, dto));
+    }
+
+    @DeleteMapping("/{ownerId}")
+    public ResponseEntity<?> deleteOwner(
+            @PathVariable Long ownerId,
+            @RequestBody DeleteOwnerRequestDto dto
+    ) {
+        ownerService.delete(ownerId, dto);
+        return ResponseEntity.ok().build();
+    }
 }
