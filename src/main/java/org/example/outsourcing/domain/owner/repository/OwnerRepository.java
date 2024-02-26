279a2d96b20d5db3da3d7cc0d96eb0c7c2ab7b6a
yallo428 <yallo428@naver.com>
2024-02-26 15:08:38 +0900

    feat : findByEmail

--- src/main/java/org/example/outsourcing/domain/owner/repository/OwnerRepository.java
+++ src/main/java/org/example/outsourcing/domain/owner/repository/OwnerRepository.java
@@ -3,5 +3,8 @@ package org.example.outsourcing.domain.owner.repository;
 import org.example.outsourcing.domain.owner.entity.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
+    Optional<Owner> findByEmail(String email);
 }
