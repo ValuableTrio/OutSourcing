ff1ed80488900cab79bc03154dfd40231687a727
Modafi <dkdpdldjdy0619@gmail.com>
2024-02-26 21:18:47 +0900

    impl: Owner jpa repository interface

--- src/main/java/com/sparta/outsourcing/repository/OwnerRepository.java
+++ src/main/java/com/sparta/outsourcing/repository/OwnerRepository.java
@@ -1,4 +1,10 @@
 package com.sparta.outsourcing.repository;
 
-public interface OwnerRepository {
+import com.sparta.outsourcing.entity.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
+    Optional<Owner> findByEmail(String email);
 }
