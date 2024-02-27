0b6eff3820a474ce4bd72c10323c812b4fd585e0
Modafi <155048724+Modafi@users.noreply.github.com>
2024-02-27 14:48:01 +0900

    Revert "impl: update UserController"
    This reverts commit 9e3730722c7295bbf8e518c68c891c79971a57a1.

--- src/main/java/com/sparta/outsourcing/controller/UserController.java
+++ src/main/java/com/sparta/outsourcing/controller/UserController.java
@@ -2,7 +2,6 @@ package com.sparta.outsourcing.controller;
 
 import com.sparta.outsourcing.dto.user.*;
 import com.sparta.outsourcing.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,11 +19,8 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(
-            @RequestBody LoginUserRequestDto dto,
-            HttpServletRequest request
-    ) {
-        return ResponseEntity.ok().body(userService.login(dto, request));
+    public ResponseEntity<?> login(@RequestBody LoginUserRequestDto dto) {
+        return ResponseEntity.ok().body(userService.login(dto));
     }
 
     @PutMapping("/{memberId}")
@@ -39,7 +35,7 @@ public class UserController {
     public ResponseEntity<?> delete(
             @PathVariable("memberId") Long memberId,
             @RequestBody DeleteUserRequestDto dto
-    ) {
+    ){
         userService.delete(memberId, dto);
         return ResponseEntity.ok().build();
     }
