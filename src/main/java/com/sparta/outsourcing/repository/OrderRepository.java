8fe8648d7e2493128b44aeb3784a177a27efaa03
ParkDongSeob <bagd0445@gmail.com>
2024-02-27 16:40:27 +0900

    refactor: Optimize database queries in OrderRepository

--- src/main/java/com/sparta/outsourcing/repository/OrderRepository.java
+++ src/main/java/com/sparta/outsourcing/repository/OrderRepository.java
@@ -1,4 +1,10 @@
 package com.sparta.outsourcing.repository;
 
-public interface OrderRepository {
+import com.sparta.outsourcing.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByStoreId(Long storeId);
 }
