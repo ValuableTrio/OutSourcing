e2042aaf20928c884c04a3b4c57e290430a3f84a
yallo428 <yallo428@naver.com>
2024-02-27 14:54:35 +0900

    add DibsRepository

---
+++ src/main/java/com/sparta/outsourcing/repository/DibsRepository.java
@@ -0,0 +1,8 @@
+package com.sparta.outsourcing.repository;
+
+
+import com.sparta.outsourcing.entity.Dibs;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DibsRepository extends JpaRepository<Dibs, Long> {
+}
