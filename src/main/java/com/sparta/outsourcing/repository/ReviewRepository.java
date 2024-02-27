cc617b20aeb51fa8fbcfc8a8f63da244ab93d3d8
ParkDongSeob <bagd0445@gmail.com>
2024-02-27 16:35:17 +0900

    fix: Update ReviewRepository to optimize database queries

--- src/main/java/com/sparta/outsourcing/repository/ReviewRepository.java
+++ src/main/java/com/sparta/outsourcing/repository/ReviewRepository.java
@@ -1,4 +1,10 @@
 package com.sparta.outsourcing.repository;
 
-public interface ReviewRepository {
+import com.sparta.outsourcing.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByStoreId(Long storeId);
 }
